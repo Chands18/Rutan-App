e0744d2db0aaff0ae91e4d7f59ef029236646e3f
Faridz <faridzkurniaa@gmail.com>
2021-08-07 17:32:26 +0700

    add tab navigator

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,22 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
 import {StyleSheet} from 'react-native';
-import {SignIn, SplashScreen} from '../pages';
+import {Akun, Beranda, Pekerjaan, Pesan, SignIn, SplashScreen} from '../pages';
 
 const Stack = createNativeStackNavigator();
-// const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return(
+    <Tab.Navigator>
+      <Tab.Screen name="Beranda" component={Beranda} />
+      <Tab.Screen name="Pekerjaan" component={Pekerjaan} />
+      <Tab.Screen name="Pesan" component={Pesan} />
+      <Tab.Screen name="Akun" component={Akun} />
+    </Tab.Navigator>
+  )
+}
 
 const Router = () => {
   return (
@@ -17,6 +29,11 @@ const Router = () => {
       <Stack.Screen
         name="SignIn"
         component={SignIn}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="MainApp"
+        component={MainApp}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
