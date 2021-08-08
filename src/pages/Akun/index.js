4a285870424dfbb51ded2317bc2b18e65a02dc56
Faridz <faridzkurniaa@gmail.com>
2021-08-08 15:00:04 +0700

    data diri pages

--- src/pages/Akun/index.js
+++ src/pages/Akun/index.js
@@ -1,14 +1,115 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {IcExit, IcPerson, IcTask, ProfileDummy} from '../../assets';
+import {Gap} from '../../components/atoms';
 
-const Akun = () => {
-    return (
-        <View>
-            <Text>ini akun</Text>
+const Akun = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Text style={styles.back}>Back</Text>
+        </TouchableOpacity>
+        <Gap height={20} />
+        <Text style={styles.akun}>Akun</Text>
+        <View style={styles.photo}>
+          <View style={styles.borderPhoto}>
+            <Image source={ProfileDummy} />
+          </View>
         </View>
-    )
-}
+        <Text style={styles.firstName}>Reza</Text>
+        <Text style={styles.lastName}>Rahardian</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('DataDiri')}>
+          <View style={styles.dataDiri}>
+            <View style={styles.icDatadiri}>
+              <IcPerson />
+            </View>
+            <Text style={styles.text}>Data diri</Text>
+          </View>
+        </TouchableOpacity>
+        <View style={styles.dataDiri}>
+          <View style={styles.icDatadiri}>
+            <IcTask />
+          </View>
+          <Text style={styles.text}>Panduan</Text>
+        </View>
+        <View style={styles.dataDiri}>
+          <View style={styles.icExit}>
+            <IcExit />
+            <Text style={styles.text}>Keluar</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
 
-export default Akun
+export default Akun;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 10,
+  },
+  header: {
+    padding: 20,
+  },
+  back: {
+    color: '#D32421',
+  },
+  akun: {
+    fontSize: 20,
+    color: '#2E2D98',
+    fontWeight: 'bold',
+  },
+  borderPhoto: {
+    borderWidth: 1,
+    borderColor: '#8D92A3',
+    width: 100,
+    height: 100,
+    borderRadius: 30,
+    borderStyle: 'dashed',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  photo: {
+    marginTop: 5,
+    marginBottom: 10,
+    padding: 30,
+  },
+  firstName: {
+    fontWeight: 'bold',
+    fontSize: 40,
+    fontFamily: 'Poppins-Medium',
+  },
+  lastName: {
+    fontSize: 40,
+    fontFamily: 'Poppins-Light',
+  },
+  dataDiri: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 20,
+  },
+  icDatadiri: {
+    borderRadius: 8,
+    height: 50,
+    width: 50,
+    backgroundColor: '#F2F6FF',
+    marginRight: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontFamily: 'Poppins-Medium',
+  },
+  icExit: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 8,
+    height: 50,
+    width: 120,
+    backgroundColor: '#F2F6FF',
+    justifyContent: 'space-evenly',
+  },
+});
--- src/pages/DataDiri/index.js
+++ src/pages/DataDiri/index.js
@@ -1,14 +1,76 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {DataDiriDummy} from '../../assets';
+import {Gap} from '../../components/atoms';
 
-const DataDiri = () => {
-    return (
-        <View>
-            <Text>datadiri</Text>
+const DataDiri = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={DataDiriDummy} style={styles.cover}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Text style={styles.back}>Back</Text>
+        </TouchableOpacity>
+      </ImageBackground>
+      <View style={styles.content}>
+        <View style={styles.mainContent}>
+          <Text style={styles.name}>Reza Rahardian</Text>
+          <Gap height={20} />
+          <Text style={styles.title}>No. ID</Text>
+          <Text style={styles.subtitle}>03-8568-00</Text>
+          <Gap height={20} />
+          <Text style={styles.title}>Telp</Text>
+          <Text style={styles.subtitle}>081234567890</Text>
+          <Gap height={20} />
+          <Text style={styles.title}>Cabang</Text>
+          <Text style={styles.subtitle}>Makassar</Text>
         </View>
-    )
-}
+      </View>
+    </View>
+  );
+};
 
-export default DataDiri
+export default DataDiri;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  back: {
+    color: '#D32421',
+    paddingTop: 26,
+    paddingLeft: 22,
+  },
+  cover: {
+    height: 330,
+  },
+  content: {
+    borderTopRightRadius: 40,
+    borderTopLeftRadius: 40,
+    marginTop: -30,
+    backgroundColor: '#F2F6FF',
+    paddingTop: 26,
+    paddingHorizontal: 16,
+    flex: 1,
+  },
+  mainContent: {
+    padding: 20,
+  },
+  name: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: 21,
+  },
+  title: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 13,
+  },
+  subtitle: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: 16,
+  },
+});
